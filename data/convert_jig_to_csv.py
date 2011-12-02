c9738873ca60a734e0bae00ead8bf8d33411e76d
Hamilton Turner <hamiltont@gmail.com>
2011-12-02 12:07:18 -0500

    Escaping commas

--- data/convert_jig_to_csv.py
+++ data/convert_jig_to_csv.py
@@ -9,11 +9,11 @@ last_doc_id = -1
 
 for line in jig:
 	if "<docID>" in line:
-		last_doc_id = line[11:-9]
+		last_doc_id = line[11:-9].replace(',','%2C')
 	if "<docText>" in line:
 		csv.write(last_doc_id)
 		csv.write(',')
-		csv.write(line[13:-11])
+		csv.write(line[13:-11].replace(',','%2C'))
 		csv.write('\n')
 
 csv.close()
